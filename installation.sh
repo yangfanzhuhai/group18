500c92b5e90d137ca1e2e338fe3e754910a38b85
Colin Stannard <colin.stannard11@imperial.ac.uk>
2013-10-16 16:05:41 +0100

    Initialises Play framework with a new Java application. Tested - launches simple application.

--- installation.sh
+++ installation.sh
@@ -7,3 +7,15 @@ sudo apt-get install -y aptitude
 # Upgrade system
 sudo aptitude update
 sudo aptitude -y upgrade
+
+# Install Java
+sudo aptitude purge openjdk*
+sudo add-apt-repository ppa:webupd8team/java
+sudo aptitude update
+sudo aptitude install oracle-java7-installer
+
+# Install Jenkins
+#cd jenkins
+#sudo sh installation.sh
+
+
