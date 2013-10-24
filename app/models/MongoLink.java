0232952d833b3eef636d8bb5cf78c828c6ddd88d
Piotr Tokaj <corona29.doc.ic.ac.uk>
2013-10-24 18:54:26 +0100

    Change getNews to return an array of arrays of String, using ActivityModel

--- app/models/MongoLink.java
+++ app/models/MongoLink.java
@@ -2,11 +2,14 @@ package models;
 
 import java.awt.List;
 import java.net.UnknownHostException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Set;
 
 import org.bson.BasicBSONObject;
 import org.bson.types.ObjectId;
+
+import play.api.libs.json.Json;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -44,9 +47,9 @@ public class MongoLink {
 		//boolean auth = db.authenticate(DBUSER, DBPASS.toCharArray());
 
 		//Prints last 20 items of newsFeed
-		ArrayList<ArrayList<DBObject>> list = ml.getNewsFeed(20);
-		for(ArrayList<DBObject> a : list) {
-			for(DBObject o : a) {
+		ArrayList<ArrayList<String>> list = ml.getNewsFeed(20);
+		for(ArrayList<String> a : list) {
+			for(String o : a) {
 				System.out.println(o);
 			}
 		}
@@ -65,24 +68,29 @@ public class MongoLink {
 	}
 
 	//returns the last postLimit posts with replies
-	public ArrayList<ArrayList<DBObject>> getNewsFeed(int postLimit) {
+	public ArrayList<ArrayList<String>> getNewsFeed(int postLimit) {
 
 		ArrayList<DBObject> posts = (ArrayList<DBObject>) newsFeed.find(new BasicDBObject("target", "")).sort(new BasicDBObject("_id", -1)).limit(postLimit).toArray();
-		ArrayList<ArrayList<DBObject>> list = new ArrayList<ArrayList<DBObject>>();
+		ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
 		
-		int i = 0;
-		while(i < posts.size())
-		{
-			ArrayList<DBObject> replies = getReply(posts.get(i).get("_id").toString());
-			replies.add(0, posts.get(i));
-			list.add(i, replies);
-			i++;
+		try {
+			int i = 0;
+			while(i < posts.size())
+			{
+				ArrayList<String> replies = getReply(posts.get(i).get("_id").toString());
+				replies.add(0, new ActivityModel(posts.get(i).toString()).toJSON());
+				list.add(i, replies);
+				i++;
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return list;
 	}
 	
-	public ArrayList<ArrayList<DBObject>> getNewsFeed(){
+	public ArrayList<ArrayList<String>> getNewsFeed(){
 		return getNewsFeed(20);
 	}
 	
@@ -105,13 +113,17 @@ public class MongoLink {
 		return news;
 	}
 	
-	private static ArrayList<DBObject> getReply(String id) {
+	private static ArrayList<String> getReply(String id) throws ParseException {
 		
 		ArrayList<DBObject> list = (ArrayList<DBObject>) newsFeed.find(new BasicDBObject("target", id)).toArray();
+		ArrayList<String> retList = new ArrayList<String>();		
 		
-		if(!list.isEmpty())
-			list.addAll(getReply(list.get(0).get("_id").toString()));
 		
-		return list;
+		if(!list.isEmpty()) {
+			retList.add(new ActivityModel(list.get(0).toString()).toJSON());
+			retList.addAll(getReply(list.get(0).get("_id").toString()));
+		}
+		
+		return retList;
 	}
 }
