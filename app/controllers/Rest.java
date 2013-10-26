78d47359ea98dc90427ac2dbdd8c90cfbaac1dc6
Luke Shilleto <luke.shilleto11@imperial.ac.uk>
2013-10-26 23:32:30 +0100

    Added *.scale.html template files and ned routes

--- app/controllers/Rest.java
+++ app/controllers/Rest.java
@@ -1,23 +1,36 @@
 package controllers;
 
+import java.net.UnknownHostException;
 import java.util.Map;
 
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
 import models.ActivityModel;
+import models.MongoLink;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Rest extends Controller {
-  
-  public static Result createMessage() {
-    final Map<String, String[]> values = request().body().asFormUrlEncoded();
-    String activityJson = values.get("activity")[0];
-    
-    try {
-      ActivityModel activity = new ActivityModel(activityJson);
-      return ok(activity.toJSON());
-    } catch (Exception e){
-      return status(400);
-    }
-  }
-  
+
+	public static Result createMessage() {
+		final Map<String, String[]> values = request().body()
+				.asFormUrlEncoded();
+		String activityJson = values.get("activity")[0];
+
+		try {
+			MongoLink mongoLink = new MongoLink();
+			ActivityModel activity = new ActivityModel(activityJson);
+			mongoLink.insertNews((DBObject)JSON.parse(activity.toJSON()));
+			return ok(activity.toJSON());
+		} catch (Exception e) {
+			return status(400);
+		}
+	}
+	
+	 public static Result getActivities() throws UnknownHostException {
+	    	MongoLink mongoLink = new MongoLink();
+	        return ok(mongoLink.getNewsFeed(3).toString());
+	    }
+
 }
