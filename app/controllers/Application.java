ea5e4616b5d8f99dceacaa5d37f938518f0d75b8
Luke Shilleto <luke.shilleto11@imperial.ac.uk>
2013-10-28 12:21:44 +0000

    Finished logins

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,39 +7,54 @@ import views.html.builds;
 import views.html.gits;
 import views.html.index;
 import views.html.login;
-import views.html.tasks;
 import views.html.register;
-
+import views.html.tasks;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render());
-    }
-    
-    public static Result about() {
-        return ok(about.render());
-    }
-    
-    public static Result builds() {
-        return ok(builds.render());
-    }
-    
-    public static Result gits() {
-        return ok(gits.render());
-    }
-    
-    public static Result login() {
-        return ok(login.render());
-    }
-    
-    public static Result tasks() {
-        return ok(tasks.render());
-    }
-    
-    public static Result register() {
-        return ok(register.render());
-    }
-    
+	public static Result index() {
+		if (session("connected") != null) {
+			return ok(index.render());
+		} else {
+			return ok(login.render());
+		}
+	}
+
+	public static Result about() {
+		return ok(about.render());
+	}
+
+	public static Result builds() {
+		if (session("connected") != null) {
+			return ok(builds.render());
+		} else {
+			return ok(login.render());
+		}
+	}
+
+	public static Result gits() {
+		if (session("connected") != null) {
+			return ok(gits.render());
+		} else {
+			return ok(login.render());
+		}
+	}
+
+	public static Result login() {
+		session().clear();
+		return ok(login.render());
+	}
+
+	public static Result tasks() {
+		if (session("connected") != null) {
+			return ok(tasks.render());
+		} else {
+			return ok(login.render());
+		}
+	}
+
+	public static Result register() {
+		return ok(register.render());
+	}
 
 }
