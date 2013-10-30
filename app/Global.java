16844bbe2d384efa28ea98b87cd6e29feb9062a4
Luke Shilleto <luke.shilleto11@imperial.ac.uk>
2013-10-30 18:34:27 +0000

    Added a dev mode the MongoLink

--- app/Global.java
+++ app/Global.java
@@ -3,6 +3,7 @@ import java.net.UnknownHostException;
 import models.MongoLink;
 import play.Application;
 import play.GlobalSettings;
+import play.api.Play;
 
 
 public class Global extends GlobalSettings {
@@ -11,6 +12,7 @@ public class Global extends GlobalSettings {
 	  @Override
 	  public void onStart(Application app) {
 	    try {
+	    	Play.isDev(Play.current());
 			MongoLink.MONGO_LINK = new MongoLink();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
