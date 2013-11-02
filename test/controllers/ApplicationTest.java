b9bdbec804b1181d6b2d4fb201d8a520d4dcb9aa
Luke Shilleto <luke.shilleto11@imperial.ac.uk>
2013-11-02 00:38:52 +0000

    Revert "Breaking the build on purpose"
    This reverts commit ac9871ccc2ceb087b158ee8c3cccd69bee27e390.

--- test/controllers/ApplicationTest.java
+++ test/controllers/ApplicationTest.java
@@ -15,7 +15,7 @@ public class ApplicationTest {
     @Test
     public void simpleCheck() {
         int a = 1 + 1;
-        assertThat(a).isEqualTo(3);
+        assertThat(a).isEqualTo(2);
     }
 
 }
